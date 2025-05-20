3dd0379c9e7bf49c2aba46eb0ad6e4c811b0131d
Xhovana Ndreu <xhovanandreu@gmail.com>
2025-05-20 14:38:06 +0200

    fix

---
+++ app/Listeners/LogSuccessfulLogin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+//use IlluminateAuthEventsLogin;
+use Illuminate\Auth\Events\Login as IlluminateAuthEventsLogin;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+class LogSuccessfulLogin
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(IlluminateAuthEventsLogin $event): void
+    {
+        $event->user->increment("login_count");
+    }
+}
