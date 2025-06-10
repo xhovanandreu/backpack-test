1160e500f844e2fe2a5dce64e24e5a3699c66019
Xhovana Ndreu <xhovanandreu@gmail.com>
2025-06-10 09:19:19 +0200

    updates

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\ApplicationUserServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     Maatwebsite\Excel\ExcelServiceProvider::class,
 ];
